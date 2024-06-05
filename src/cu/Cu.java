e7b139446eeecae33ddb73bd7b54b3dc4681fb1d
enzolang <enzolanglang789@gmail.com>
2024-06-04 21:52:36 -0300

    iniciando projeto

---
+++ src/cu/Cu.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cu;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author light
+ */
+public class Cu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc= new Scanner (System.in);
+        String nome;
+        
+        System.out.println("qual o tamanho do seu penis");
+        nome=sc.next();
+        
+        System.out.println("parabens, que palzao " + nome);
+        
+        
+        
+        
+
+    }
+    
+}
